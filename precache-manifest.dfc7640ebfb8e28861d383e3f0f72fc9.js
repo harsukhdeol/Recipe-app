67aabcecec974827c1ac8afb5a0cd723608db0bb
sukhd <ed@email.com>
2019-07-30 22:36:16 -0500

    Updates

---
+++ precache-manifest.dfc7640ebfb8e28861d383e3f0f72fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38670927d18a62ba78df8eb66866386f",
+    "url": "/Recipe-app/index.html"
+  },
+  {
+    "revision": "1631c1e2c02971feaf2a",
+    "url": "/Recipe-app/static/css/main.a1d4b169.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/Recipe-app/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "1631c1e2c02971feaf2a",
+    "url": "/Recipe-app/static/js/main.ec65d52c.chunk.js"
+  },
+  {
+    "revision": "182a6d6796f271f84e75",
+    "url": "/Recipe-app/static/js/runtime~main.d807f277.js"
+  }
+]);
